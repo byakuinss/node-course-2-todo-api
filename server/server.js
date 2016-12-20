e9b7193ca117665331ef24ba0b7c11f7200e6840
Grace <grace.chinhanyu@gmail.com>
2016-12-20 23:21:46 +0800

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -23,9 +23,12 @@ app.post('/todos', (req, res) => {
 	})
 });
 
-app.get('/getall', (req, res) => {
-	console.log(req.body);
-	res.send('You got me!');
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	})
 })
 
 
