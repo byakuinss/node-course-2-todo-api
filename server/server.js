7411742e16ad9d1c8e37c8f0e7c7729e6816fb3d
Grace <grace.chinhanyu@gmail.com>
2016-12-26 16:55:34 +0800

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config.js');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser'); //take json to request object
@@ -8,7 +10,8 @@ var {User} = require('./modules/user');
 var {ObjectID} = require('mongodb');
 
 var app = express();
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json()); 
 
